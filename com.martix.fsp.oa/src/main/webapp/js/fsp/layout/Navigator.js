9b3d58fc505d0cc2071e3b0cf8cea048b39cb7e0
hopesfish <hopesfish@163.com>
2013-04-30 16:46:38 +0800

    完成navigator的完善

--- com.martix.fsp.oa/src/main/webapp/js/fsp/layout/Navigator.js
+++ com.martix.fsp.oa/src/main/webapp/js/fsp/layout/Navigator.js
@@ -8,22 +8,24 @@ define([
     "dijit/_TemplatedMixin",
     "dijit/_WidgetsInTemplateMixin",
     'dojo/text!./templates/Navigator.html',
-    "dijit/layout/AccordionContainer"
+    "dijit/layout/AccordionContainer",
+    "dojo/store/Memory",
+    "dijit/tree/ObjectStoreModel",
+    "dijit/Tree"
 ], function(
     declare, lang, on,
     ContentPane, TemplatedMixin, WidgetsInTemplateMixin, template,
-    AccordionContainer) {
+    AccordionContainer, Memory, ObjectStoreModel, Tree) {
 
 
     return declare("fsp.layout.Navigator", [ContentPane, TemplatedMixin, WidgetsInTemplateMixin], {
         templateString: template,
-        
+        aContainer: null,
         postCreate: function() {
-            var aContainer = new AccordionContainer({style:"height: 100%"}, this.navNode);
-            aContainer.addChild(new ContentPane({
-                title:"OA任务",
-                content:"模板管理菜单"
-            }));
+            var aContainer;
+            aContainer = this.aContainer = new AccordionContainer({style:"height: 100%"}, this.navNode);
+            
+            this.addOALinks();
             aContainer.addChild(new ContentPane({
                 title:"模板管理",
                 content:"模板管理菜单"
@@ -33,6 +35,44 @@ define([
                 content:"系统管理菜单"
             }));
             aContainer.startup();
+        },
+
+        addOALinks: function() {
+            var oaStore, oaModel, oaPane, oaTree;
+
+            oaStore = new Memory({
+                data: [
+                    { id: 'root', name:'常用功能', link: ""},
+                    { id: 'task', name:'代办任务', link: "", parent: 'root'}
+                ],
+                getChildren: function(object){
+                    return this.query({parent: object.id});
+                }
+            });
+
+            oaModel = new ObjectStoreModel({
+                store: oaStore,
+                query: {id: 'root'}
+            });
+
+            oaPane = new ContentPane({
+                title:"OA任务"
+            });
+
+            oaTree = new Tree({
+                model: oaModel,
+                onClick: lang.hitch(this, this.onClick)
+            });
+
+            this.aContainer.addChild(oaPane);
+
+            setTimeout(function() {
+                oaPane.set("content", oaTree);
+            }, 500);
+        },
+
+        onClick: function(item) {
+            console.info(item);
         }
     });
 });
